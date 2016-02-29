3783015fda1b34f3a9c2229166681d7fb4c1bcff
Artem V. Navrotskiy <bozaro@yandex.ru>
2016-02-29 22:29:33 +0300

    Example of WebDav request

--- main.go
+++ main.go
@@ -3,12 +3,44 @@
 package main
 
 import (
+	"bytes"
+	"flag"
 	"fmt"
 	"github.com/bozaro/pdbuploader/parse"
+	"net/http"
 	"os"
 )
 
+func upload(username string, password string) {
+	client := &http.Client{
+	//    CheckRedirect: redirectPolicyFunc,
+	}
+	/*req, err := http.NewRequest("HEAD", "https://webdav.yandex.ru/PDB/test.txt", nil)
+	req.SetBasicAuth(username, password)
+	resp, err := client.Do(req)
+	fmt.Println(err)
+	fmt.Println(resp)*/
+
+	propfind := []byte("<?xml version=\"1.0\"?><propfind xmlns=\"DAV:\"><propname/></propfind>")
+	req, err := http.NewRequest("PROPFIND", "https://webdav.yandex.ru/PDB", bytes.NewReader(propfind))
+	req.Header.Set("Content-Type", "application/xml")
+	req.Header.Set("Content-Length", fmt.Sprintf("%d", len(propfind)))
+	req.SetBasicAuth(username, password)
+
+	resp, err := client.Do(req)
+	fmt.Println(err)
+	fmt.Println(resp)
+
+}
+
 func main() {
+
+	usernamePtr := flag.String("username", "bozaro", "Username")
+	passwordPtr := flag.String("password", "", "Password")
+	flag.Parse()
+
+	upload(*usernamePtr, *passwordPtr)
+
 	file, _ := os.Open("sample/hello.exe")
 	{
 		info, err := parse.ParseExe(file)
