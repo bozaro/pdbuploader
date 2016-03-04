5d692dafbd9f329ec9c78f2230b6b2575487b8fa
Artem V. Navrotskiy <bozaro@yandex.ru>
2016-03-04 07:22:15 +0300

    Add wildcard tests

---
+++ wildcard/normalize_test.go
@@ -0,0 +1,61 @@
+package wildcard
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type normalizePatternData struct {
+	pattern  string
+	expected []string
+}
+
+func (this normalizePatternData) String() string {
+	return this.pattern
+}
+
+func TestNormalizePattern(t *testing.T) {
+	data := []normalizePatternData{
+		// Simple mask
+		normalizePatternData{"/", []string{"**/"}},
+		normalizePatternData{"*/", []string{"*/", "**/"}},
+		normalizePatternData{"*", []string{"*"}},
+		normalizePatternData{"**", []string{}},
+		normalizePatternData{"**/", []string{"**/"}},
+		normalizePatternData{"foo", []string{"**/", "foo"}},
+		normalizePatternData{"foo/", []string{"**/", "foo/"}},
+		normalizePatternData{"/foo", []string{"foo"}},
+
+		// Convert path file mask
+		normalizePatternData{"foo/**.bar", []string{"foo/", "**/", "*.bar"}},
+		normalizePatternData{"foo/***.bar", []string{"foo/", "**/", "*.bar"}},
+
+		// Collapse and reorder adjacent masks
+		normalizePatternData{"foo/*/bar", []string{"foo/", "*/", "bar"}},
+		normalizePatternData{"foo/**/bar", []string{"foo/", "**/", "bar"}},
+		normalizePatternData{"foo/*/*/bar", []string{"foo/", "*/", "*/", "bar"}},
+		normalizePatternData{"foo/**/*/bar", []string{"foo/", "*/", "**/", "bar"}},
+		normalizePatternData{"foo/*/**/bar", []string{"foo/", "*/", "**/", "bar"}},
+		normalizePatternData{"foo/*/**.bar", []string{"foo/", "*/", "**/", "*.bar"}},
+		normalizePatternData{"foo/**/**/bar", []string{"foo/", "**/", "bar"}},
+		normalizePatternData{"foo/**/**.bar", []string{"foo/", "**/", "*.bar"}},
+		normalizePatternData{"foo/**/*/**/*/bar", []string{"foo/", "*/", "*/", "**/", "bar"}},
+		normalizePatternData{"foo/**/*/**/*/**.bar", []string{"foo/", "*/", "*/", "**/", "*.bar"}},
+
+		// Collapse trailing masks
+		normalizePatternData{"foo/**", []string{"foo/"}},
+		normalizePatternData{"foo/**/*", []string{"foo/", "*"}},
+		normalizePatternData{"foo/**/*/*", []string{"foo/", "*/", "*"}},
+		normalizePatternData{"foo/**/", []string{"foo/", "**/"}},
+		normalizePatternData{"foo/**/*/", []string{"foo/", "*/", "**/"}},
+		normalizePatternData{"foo/**/*/*/", []string{"foo/", "*/", "*/", "**/"}},
+	}
+	for _, value := range data {
+		normalizePatternCheck(t, &value)
+	}
+}
+
+func normalizePatternCheck(t *testing.T, v *normalizePatternData) {
+	actual := NormalizePattern(SplitPattern(v.pattern))
+	assert.Equal(t, v.expected, actual, v.String())
+}
---
+++ wildcard/wildcard.go
@@ -0,0 +1,9 @@
+package wildcard
+
+func SplitPattern(path string) []string {
+	return []string{}
+}
+
+func NormalizePattern(tokens []string) []string {
+	return tokens
+}
