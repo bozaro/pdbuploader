252e0fd94fc67695a8747b1c8664a4e3ad61e1e9
Artem V. Navrotskiy <bozaro@yandex.ru>
2016-03-04 13:49:17 +0300

    wildcard: SplitPattern

--- wildcard/wildcard.go
+++ wildcard/wildcard.go
@@ -1,7 +1,26 @@
 package wildcard
 
+const PATH_SEPARATOR rune = '/'
+
+/**
+  Split pattern with saving slashes.
+
+  @param pattern Path pattern.
+  @return Path pattern items.
+*/
 func SplitPattern(path string) []string {
-	return []string{}
+	result := []string{}
+	start := 0
+	for i, c := range path {
+		if c == PATH_SEPARATOR {
+			result = append(result, path[start:i+1])
+			start = i + 1
+		}
+	}
+	if start != len(path) {
+		result = append(result, path[start:])
+	}
+	return result
 }
 
 func NormalizePattern(tokens []string) []string {
