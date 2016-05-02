456cc7a2a50eeaee235deae109077035c32d0d4d
Artem V. Navrotskiy <bozaro@yandex.ru>
2016-05-02 07:45:38 +0300

    Update tests

--- parse/parse_exe_test.go
+++ parse/parse_exe_test.go
@@ -6,20 +6,38 @@ import (
 	"testing"
 )
 
-func TestParseExe(t *testing.T) {
-	file, _ := os.Open("../sample/hello.exe")
+func TestParseExe32(t *testing.T) {
+	file, _ := os.Open("../sample/hello32.exe")
 	info, err := ParseExe(file)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	assert.Equal(t, "C:\\Work\\github\\octobuild\\test_cl\\hello.pdb", info.PDBFileName)
+	assert.Equal(t, "C:\\Bozaro\\pdbuploader\\sample\\hello32.pdb", info.PDBFileName)
 	// Code Id
-	assert.Equal(t, int32(0x56D047B1), info.CodeId.TimeDateStamp)
-	assert.Equal(t, int32(0x2e000), info.CodeId.SizeOfImage)
-	assert.Equal(t, "56D047B12e000", info.CodeId.String())
+	assert.Equal(t, int32(0x57218B93), info.CodeId.TimeDateStamp)
+	assert.Equal(t, int32(0x65000), info.CodeId.SizeOfImage)
+	assert.Equal(t, "57218B9365000", info.CodeId.String())
 	// Debug Id
-	assert.Equal(t, "B3963800A43840D28914331E6B93FE02", info.DebugId.Guid.String())
-	assert.Equal(t, 1, info.DebugId.TimeDateStamp)
-	assert.Equal(t, "B3963800A43840D28914331E6B93FE021", info.DebugId.String())
+	assert.Equal(t, "74219E6FC19941AAB1812BA7E7FF1EFE", info.DebugId.Guid.String())
+	assert.Equal(t, 2, info.DebugId.TimeDateStamp)
+	assert.Equal(t, "74219E6FC19941AAB1812BA7E7FF1EFE2", info.DebugId.String())
+}
+
+func TestParseExe64(t *testing.T) {
+	file, _ := os.Open("../sample/hello64.exe")
+	info, err := ParseExe(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, "C:\\Bozaro\\pdbuploader\\sample\\hello64.pdb", info.PDBFileName)
+	// Code Id
+	assert.Equal(t, int32(0x57218B96), info.CodeId.TimeDateStamp)
+	assert.Equal(t, int32(0x7e000), info.CodeId.SizeOfImage)
+	assert.Equal(t, "57218B967e000", info.CodeId.String())
+	// Debug Id
+	assert.Equal(t, "B39838386DAE42DF9F1E6B0B12963079", info.DebugId.Guid.String())
+	assert.Equal(t, 2, info.DebugId.TimeDateStamp)
+	assert.Equal(t, "B39838386DAE42DF9F1E6B0B129630792", info.DebugId.String())
 }
